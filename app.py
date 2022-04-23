b11f6a40a58c4beb779744c4335484812afb48ac
Eduardo Perottoni <edu.perottoni@gmail.com>
2022-04-23 09:18:08 -0300

    Descrição do membro 1, adição do membro 4 e ajuste de exibição no app

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import membro1 as m1
 import membro2 as m2
 import membro3 as m3
+import membro4 as m4
 
 print(m1.hello())
+print('-'*10)
 print(m2.hello())
+print('-'*10)
 print(m3.hello())
+print('-'*10)
+print(m4.hello())
--- membro1.py
+++ membro1.py
@@ -1,2 +1,4 @@
 def hello():
-	return "Olá, eu sou o membro1" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	#nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return 'Olá, eu sou Eduardo Perottoni, tenho 20 anos e gosto muito de praticar esportes (voleibol, handebol e futebol) no meu tempo livre, assim como ler. \nMinha experiência com programação é mais voltada ao Desenvolvimento Web front-end (HTML, CSS, JavaScript, Svelte) e veio de alguns cursos online que fiz. \nSobre a disciplina, as expectativas são grandes e espero conhecer a galera, colocar muito a "mão na massa" e aprender sobre esse paradigma importantíssimo para a programação.' 
+	
