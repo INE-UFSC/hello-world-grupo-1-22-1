9fa45bf23d23fdd14b157ab3e0c0d19de260b23f
Pablo <xpspablo@hotmail.com>
2022-04-24 21:01:05 -0300

    Alteração membro3.py

--- membro3.py
+++ membro3.py
@@ -1,3 +1,2 @@
 def hello():
 	return "Olá, eu sou o Pablo, tenho 19 anos e gosto muito de estudar computação, matemática e ler clássicos literários. Meu primeiro contato com programação foi com 15 anos, pois sou curioso e queria saber como uma página na internet funcionava. Espero muito aprender Orientação a Objetos e conhecer o pessoal da sala e interagir bastante."
-print(hello())
