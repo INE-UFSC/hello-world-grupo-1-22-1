997e63ca95465d449eade3a0e18cf3032e7324fb
NicolasRTFS <nicolas.rtfs@gmail.com>
2022-04-26 21:56:36 -0300

    Adicao Membro 4 (desculpa o atraso)

--- membro4.py
+++ membro4.py
@@ -1,2 +1,3 @@
 def hello():
-	return "Olá, eu sou o membro4" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+    #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Saudações, meu nome é Nícolas Rone! Tenho 22 anos e venho de São Paulo. Desde muito novo já gostava de arte mas sempre tive o minimo nescessário para passar no resto haha. Alguns dos meus hobbies são desenhar, cantar quando estou sozinho (nesse sou péssimo), esculpir, fazer longas caminhadas, confecção de cadernos e gosto de criar histórias também (não que já tenha terminado alguma haha). Tive um breve contato com programação no tecnico em eletrônica que fiz no IFSC, programando um pouco de C. Da disciplina espero aprender mais sobre orientação a objetos e também aprender a organizar melhor meus algoritmos."
